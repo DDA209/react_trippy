4b5459b89cc8454970616cfd4d24a905e6fb1aa4
DDA209 <didier.pascarel1@gmail.com>
2020-08-08 13:40:19 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
+
+import HomeContainer from './components/containers/HomeContainer';
+import TemplateContainer from './components/containers/TemplateContainer';
+
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to ="/">Trippy</Link></li>
+              <li><Link to ="/template">template</Link></li>
+              <li><Link to ="/hotels">Hotels</Link></li>
+              <li><Link to ="/restaurants">Restaurants</Link></li>
+              <li><Link to ="/login">Login</Link></li>
+              <li><Link to ="/signup">Signup</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/template">
+              <TemplateContainer />
+            </Route>
+            <Route path="/">
+              <HomeContainer />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/containers/TemplateContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class TemplateContainer extends React.Component{
+    render(){
+        return(
+            <p>cool</p>
+        )
+    }
+}
+
+export default TemplateContainer;
